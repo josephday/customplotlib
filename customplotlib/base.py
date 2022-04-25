b14029c09f549569b8050fd256f02b3128edd4cf
josephday <joe.day@strong.io>
2022-04-25 13:20:51 -0400

    Add base class which inherits everything from matplotlib

---
+++ customplotlib/base.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import matplotlib as plt
+
+class BaseMatplotlib():
+    pass
+
+for i in dir(plt):
+    setattr(BaseMatplotlib, i, getattr(plt, i))
