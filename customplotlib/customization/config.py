2c5884b46bd2ac626223ec217e63af9e73b9a831
Joseph Day <josephday@Josephs-MacBook-Pro.local>
2024-04-02 17:41:17 -0400

    Add 3d plots, pie charts, and dist plot

--- customplotlib/customization/config.py
+++ customplotlib/customization/config.py
@@ -1,20 +1,6 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
-from collections import OrderedDict
 import os
+from collections import OrderedDict
+
 
 ### basics
 FIGSIZE = (10,6)
@@ -24,9 +10,11 @@ AUTOSAVE = True
 
 ### font
 # download .ttf for your desired font and place at customplotlib/customization/fonts/
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/fonts/'       
-FONT = 'proxima_nova.ttf'
-FONTSIZE = 16
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/fonts/'
+TITLE_FONT = 'NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf'
+FONT = TITLE_FONT
+TITLE_SIZE = 15
+FONT_SIZE = 12
 LEGEND_LOC = (1.05,0.65)
 
 
@@ -34,54 +22,82 @@ LEGEND_LOC = (1.05,0.65)
 ### main colors
 MAINS = OrderedDict()
 
-MAINS['strong-blue'] = "#4C6FE7"
-MAINS['strong-dark-gray'] = "#4F4F4F"
-MAINS['strong-light-blue'] = "#B5C3FF"
-MAINS['strong-off-black'] = "#2a2a2a"
-MAINS['strong-dark-blue'] = "#3958C4"
-MAINS['strong-off-white'] = '#F4F4F4'
-
+MAINS['green'] = "#20b498"
+MAINS['light-green'] = "#89FBDB"
+MAINS['dark-green'] = "#285C6F"
+MAINS['dark-gray'] = "#575757"
+MAINS['light-gray'] = "#CCCCCC"
+MAINS['black'] = "#000000"
 
 # +
 ### supplementary colors
 SUPPS = OrderedDict()
 
-SUPPS['strong-blue'] = "#4C6FE7"
-SUPPS['strong-red'] = "#DE364F"
-SUPPS['strong-orange'] = "#FFC65F"
-SUPPS['strong-green'] = "#1FC198"
-SUPPS['strong-purple'] = '#D065D3'
-SUPPS['strong-yellow'] = '#F9F871'
-SUPPS['strong-dark-orange'] = '#D24B00'
-SUPPS['strong-dark-green'] = '#008A65'
-SUPPS['strong-light-purple']= '#A5A7DD'
-SUPPS['strong-dark-red'] = '#9F0023'
+SUPPS['green'] = "#20b498"
+SUPPS['orange'] = "#faa613"
+SUPPS['violet'] = "#52489C"
+SUPPS['blue'] = "#48acf0"
+SUPPS['pink'] = "#f06c9b"
+SUPPS['yellow'] = "#eeff41"
+SUPPS['navy'] = "#0B315E"
+SUPPS['burgundy'] = "#550527"
+SUPPS['gray'] = "#767676"
+SUPPS['red'] = "#C2203E"
+
+SUPPS['light-violet'] = "#B18CC1"
+
+SUPPS['light-navy'] = "#0A4B8C"
+SUPPS['light-teal'] = "#78EAD7"
+SUPPS['light-yellow'] = "#FFE97D"
+SUPPS['light-orange'] = "#F99F5D"
+SUPPS['light-pink'] = "#FFBDD1"
+SUPPS['light-blue'] = "#B8E3EB"
+SUPPS['light-green'] = "#89FBDB"
+SUPPS['light-gray'] = "#CCCCCC"
+
+SUPPS['dark-violet'] = "#641987"
+SUPPS['dark-navy'] = "#021F42"
+SUPPS['dark-teal'] = "#00A78E"
+SUPPS['dark-yellow'] = "#F4B822"
+SUPPS['dark-orange'] = "#F4642A"
+SUPPS['dark-pink'] = "#E46B95"
+SUPPS['dark-blue'] = "#09A7E3"
+SUPPS['dark-green'] = "#285C6F"
+SUPPS['dark-gray'] = "#575757"
+
+SUPPS['black'] = '#000000'
+SUPPS['white'] = '#FFFFFF'
+SUPPS['off-white'] = '#F7F7F7'
+
+
+OFFICIAL = OrderedDict()
+
 
 # +
 ### pre-defined gradients
-GRADIENT_LIGHT_DARK = ['strong-off-white', 'strong-light-blue', 'strong-blue', 'strong-dark-blue'] 
-GRADIENT_HOT_COLD = ['strong-dark-red', 'strong-red', 'strong-off-white','strong-light-blue', 'strong-blue', 'strong-dark-blue']
-GRADIENT_RAINBOW = ['strong-dark-red', 'strong-red', 'strong-orange', 'strong-yellow', 'strong-green', 'strong-blue', 'strong-dark-blue']
-GRADIENT_BW = ['strong-off-white', '#D0D0D0', '#808080', 'strong-dark-gray', 'strong-off-black']
+GRADIENT_LIGHT_DARK = ['off-white', '#C6FCED', 'light-green', 'green', 'dark-green']
+GRADIENT_HOT_COLD = ['red', 'pink', 'light-pink', 'off-white', 'light-blue', 'blue', 'navy']
+GRADIENT_RAINBOW = ['burgundy', 'red', 'orange', 'yellow', 'green', 'blue', 'light-navy', 'violet', 'dark-violet']
+GRADIENT_BW = ['off-white', 'light-gray', 'gray', 'dark-gray', 'black']
 
-GRADIENTS = {'strong': GRADIENT_LIGHT_DARK, 
-            'hot_cold': GRADIENT_HOT_COLD,
-            'rainbow': GRADIENT_RAINBOW,
-            'bw': GRADIENT_BW}
+GRADIENTS = {'joe_green': GRADIENT_LIGHT_DARK,
+           'hot_cold': GRADIENT_HOT_COLD,
+           'rainbow': GRADIENT_RAINBOW,
+           'bw': GRADIENT_BW}
 
 # +
 ### line styles
 
 linestyle_tuple = [
-     ('solid',                 (0, ())),
-     ('dotted',                (0, (1, 1))),
-     ('dashed',                (0, (5, 5))),
-     ('dashdotted',            (0, (3, 5, 1, 5))),
-     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
-     ('densely dotted',        (0, (1, 1))),
-     ('densely dashed',        (0, (5, 1))),
-     ('densely dashdotted',    (0, (3, 1, 1, 1))),
-     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
+    ('solid',                 (0, ())),
+    ('dotted',                (0, (1, 1))),
+    ('dashed',                (0, (5, 5))),
+    ('dashdotted',            (0, (3, 5, 1, 5))),
+    ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+    ('densely dotted',        (0, (1, 1))),
+    ('densely dashed',        (0, (5, 1))),
+    ('densely dashdotted',    (0, (3, 1, 1, 1))),
+    ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
 
 LINESTYLES = [i[1] for i in linestyle_tuple]
 
